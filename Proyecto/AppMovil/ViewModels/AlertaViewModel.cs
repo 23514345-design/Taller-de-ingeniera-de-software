c2155229a03c38e1e6e3148fe1bf83fc28c56163
JAAS <23514345@uagro.mx>
2026-04-11 20:46:18 -0600

    Add AlertaViewModel with ObservableCollection of Alertas

--- Proyecto/AppMovil/ViewModels/AlertaViewModel.cs
+++ Proyecto/AppMovil/ViewModels/AlertaViewModel.cs
@@ -1,1 +1,15 @@
+using System.Collections.ObjectModel;
 
+public class AlertaViewModel
+{
+    public ObservableCollection<Alerta> Alertas { get; set; }
+
+    public AlertaViewModel()
+    {
+        Alertas = new ObservableCollection<Alerta>()
+        {
+            new Alerta { Mensaje = "Obstáculo detectado", Distancia = 40, EsCritica = true },
+            new Alerta { Mensaje = "Sistema activo", Distancia = 0, EsCritica = false }
+        };
+    }
+}
